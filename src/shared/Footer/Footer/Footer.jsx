0f97458943c86f0bdc4632a828ada895b0459712
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-05-19 01:17:54 +0600

    addd footer

--- src/shared/Footer/Footer/Footer.jsx
+++ src/shared/Footer/Footer/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React from "react";
+import { FaMapMarkerAlt , FaHeadphonesAlt , FaEnvelopeOpenText} from "react-icons/fa";
 
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <footer className="footer p-10 bg-[#2e2e2e] text-white py-20">
         <div>
-            <h1>This is footer.</h1>
+          <img  className="w-2/4" src="https://i.ibb.co/d6jRFLd/logo-2.png" alt="" />
+          <p>STORE INFORMATION</p>
+            <div className="flex gap-3">
+                <p className="text-2xl"><FaMapMarkerAlt></FaMapMarkerAlt></p>
+                <p>Toy Car Store <br />
+                48 Boulevard Jourdan, Paris, <br />
+                United States</p>
+            </div>
+            <div className="flex items-center gap-3">
+                <p className="text-2xl"><FaHeadphonesAlt></FaHeadphonesAlt></p>
+                <p>+1234567890</p>
+            </div>
+            <div className="flex items-center gap-3">
+                <p className="text-2xl"><FaEnvelopeOpenText></FaEnvelopeOpenText></p>
+                <p>Sales@Yourshop.Com</p>
+            </div>
+            <input className="py-2 my-2 px-2 bg-black rounded" placeholder="your email" type="email" name="email" id="" />
+            <button className="btn rounded-none w-32 hover:text-white bg-white text-black">Subscribe</button>
         </div>
-    );
+        <div>
+          <span className="footer-title">PRODUCTS</span>
+          <a className="link link-hover">Prices Drop</a>
+          <a className="link link-hover">New Products</a>
+          <a className="link link-hover">Best Sales</a>
+          <a className="link link-hover">Contact Us</a>
+          <a className="link link-hover">Sitemap</a>
+        </div>
+        <div>
+          <span className="footer-title">OUR COMPANY</span>
+          <a className="link link-hover">Delivery</a>
+          <a className="link link-hover">Legal Notice</a>
+          <a className="link link-hover">About Us</a>
+          <a className="link link-hover">Secure Payment</a>
+          <a className="link link-hover">Stores</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
