0fb6290bd85113806d8b8cce60ff44f28b350f3b
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-05-21 02:21:02 +0600

    initialize delete toys from my toy page

--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -1,24 +1,56 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 import ShowMyToys from '../ShowMyToys/ShowMyToys';
+import Swal from 'sweetalert2';
 
 const MyToy = () => {
     const { user } = useContext(AuthContext);
     const [toys, setToys] = useState([]);
     const url = `http://localhost:5000/carToys?email=${user.email}`;
-    
+
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => setToys(data))
     }, [])
 
+    const handleDelete = _id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/carToys/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            const remaining = toys.filter(toy => toy._id !== _id);
+                            setToys(remaining);
+                        }
+                    })
+            }
+        })
+    }
+
     return (
         <div className='lg:container mx-auto my-20'>
             {
                 toys.map(toy => <ShowMyToys
-                key={toy._id}
-                toy={toy}
+                    key={toy._id}
+                    toy={toy}
+                    handleDelete={handleDelete}
                 ></ShowMyToys>)
             }
         </div>
--- src/pages/ShowMyToys/ShowMyToys.jsx
+++ src/pages/ShowMyToys/ShowMyToys.jsx
@@ -3,8 +3,8 @@ import '@smastrom/react-rating/style.css'
 import React from 'react';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 
-const ShowMyToys = ({ toy }) => {
-    const { seller, name, quantity, price, img, description, rating, category, email } = toy;
+const ShowMyToys = ({ toy , handleDelete}) => {
+    const { _id, seller, name, quantity, price, img, description, rating, category, email } = toy;
     return (
         <div className='my-5 flex flex-col md:flex-row gap-3 p-3  md:items-center justify-around border-2 shadow-sm'>
             <div className=' flex items-center gap-8'>
@@ -28,7 +28,7 @@ const ShowMyToys = ({ toy }) => {
             </div>
             <div className='flex flex-row md:flex-col gap-10 '>
                 <button className='text-3xl'><FaEdit></FaEdit></button>
-                <button className='text-3xl text-red-500'><FaTrashAlt></FaTrashAlt></button>
+                <button onClick={() => handleDelete(_id)} className='text-3xl text-red-500'><FaTrashAlt></FaTrashAlt></button>
             </div>
         </div>
     );
