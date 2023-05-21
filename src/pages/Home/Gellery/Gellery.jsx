de37508c7a3aefbeb656ee01ccf53a62b68117fa
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-05-21 21:26:18 +0600

    added gallery on home page

---
+++ src/pages/Home/Gellery/Gellery.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Gellery = () => {
+    return (
+        <div className='max-w-6xl mx-auto'>
+            <h3 className='text-center font-semibold text-3xl my-5'>PRODUCTS GALLERY</h3>
+            <div className=' my-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-2'>
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/HKcMfXr/4.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/YWrCXvX/3.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/gR4mSNG/6.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/MhQkVMv/8.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/88kH8NB/15.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/3cZXzRP/17.webp" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/2qbgBHY/18.webp" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/47XskTj/26.webp" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/H7k6gX2/27.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/5nsjzWw/39.jpg" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/KqfL7cz/41.webp" alt="" />
+                <img className='w-72 h-72 border-8' src="https://i.ibb.co/5nsjzWw/39.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Gellery;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,12 +4,14 @@ import Services from '../Services/Services';
 import ShopCategory from '../ShopCategory/ShopCategory';
 import useTitle from '../../../hooks/useTitle';
 import KidsToyCarVedio from '../KidsToyCarVedio/KidsToyCarVedio';
+import Gellery from '../Gellery/Gellery';
 
 const Home = () => {
     useTitle("Home")
     return (
         <div>
             <HomeSlider></HomeSlider>
+            <Gellery></Gellery>
             <Services></Services>
             <ShopCategory></ShopCategory>
             <KidsToyCarVedio></KidsToyCarVedio>
